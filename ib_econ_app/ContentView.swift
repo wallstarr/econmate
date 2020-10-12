3eb0b709f13b668daa01635348942d2ca5cf4024
Dan Blustein <dansakai0@gmail.com>
2020-10-12 12:30:11 +0900

    Initial Commit

---
+++ ib_econ_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ib_econ_app
+//
+//  Created by Dan Blustein on 2020/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ib_econ_app/ib_econ_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  ib_econ_appApp.swift
+//  ib_econ_app
+//
+//  Created by Dan Blustein on 2020/10/12.
+//
+
+import SwiftUI
+
+@main
+struct ib_econ_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
